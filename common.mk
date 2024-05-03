Evolution-XYZ-Devices/device_xiaomi_sm8350-common

sm8350-common: Build custom LiveDisplay HAL for venus and mars

common.mk
@@ -286,6 +286,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
+ifneq (,$(filter $(TARGET_PRODUCT), venus mars))
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.1-service.xiaomi_sm8350
+endif
+
 # Media
 PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor \

Tonyha7/android_device_xiaomi_sm8350-common

common.mk
@@ -286,6 +286,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
+ifneq (,$(filter $(PRODUCT_DEVICE), venus mars))
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.1-service.xiaomi_sm8350
+endif
+
 # Media
 PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor \